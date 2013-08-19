f91bb463533e3f3e7599f952ce52be3321e5364a
Chris Wiggins <chris@chriswiggins.co.nz>
2013-08-19 03:00:08 +0000

    Fix some errors - no httpsegmenter yet

--- manifests/install/yasm.pp
+++ manifests/install/yasm.pp
@@ -1,29 +1,23 @@
 class zoneminder::install::yasm {
 
-  exec {"mkdir-yasm":
-    command => "mkdir -p /usr/local/src/yasm",
-    cwd => "/usr/local/src",
-    creates => "/usr/local/src/yasm",
-    before => Exec["download-yasm"]
-  }
-
   exec { "download-yasm":
-    command => "curl $zoneminder::params::yasm_source | tar zxv -C /usr/local/src/yasm",
+    command => "curl $zoneminder::params::yasm_source | tar zxv -C /usr/local/src",
     cwd => "/usr/local/src",
+    creates => "/usr/local/src/yasm-1.2.0",
     timeout => 600,
     before => Exec["configure-yasm"]
   }
 
   exec { "configure-yasm":
-    command => "/usr/local/src/yasm/configure",
-    cwd => "/usr/local/src/yasm",
-    creates => "/usr/local/src/yasm/config.log",
+    command => "/usr/local/src/yasm-1.2.0/configure",
+    cwd => "/usr/local/src/yasm-1.2.0",
+    creates => "/usr/local/src/yasm-1.2.0/config.log",
     notify => Exec["make-yasm"]
   }
 
   exec { "make-yasm":
     command => "make -j `grep processor /proc/cpuinfo|wc -l`",
-    cwd => "/usr/local/src/yasm",
+    cwd => "/usr/local/src/yasm-1.2.0",
     timeout => 0,
     refreshonly => true,
     notify => Exec["make-install-yasm"]
@@ -31,7 +25,7 @@ class zoneminder::install::yasm {
 
   exec { "make-install-yasm":
     command => "make install",
-    cwd => "/usr/local/src/yasm",
+    cwd => "/usr/local/src/yasm-1.2.0",
     refreshonly => true,
   }
   
